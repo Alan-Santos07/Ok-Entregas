8e28b234264a3326aec6d792928117bb88c04c07
Alan da Costa Santos <44711461817@info.local>
2021-12-02 14:57:31 -0300

    adiciona back

---
+++ back/OkEntrega.webApi/OkEntrega.webApi/OkEntrega.webApi/Interfaces/ILeadRepository.cs
@@ -0,0 +1,48 @@
+﻿using OkEntrega.webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OkEntrega.webApi.Interfaces
+{
+    public interface ILeadRepository
+    {
+        /// <summary>
+        /// Lista todos os usuários
+        /// </summary>
+        /// <returns>Uma lista de usuários</returns>
+<<<<<<< HEAD
+        List<Lead> Listar();
+=======
+        List<UsuarioLead> Listar();
+>>>>>>> front-end
+
+        /// <summary>
+        /// Busca um usuário através do ID
+        /// </summary>
+<<<<<<< HEAD
+        Lead BuscarPorId(int id);
+
+        void Cadastrar(Lead novoLead);
+=======
+        UsuarioLead BuscarPorId(int id);
+
+        void Cadastrar(UsuarioLead novoLead);
+>>>>>>> front-end
+
+        /// <summary>
+        /// Atualiza um usuário existente
+        /// </summary>
+<<<<<<< HEAD
+        void Atualizar(int id, Lead leadAtualizado);
+=======
+        void Atualizar(int id, UsuarioLead leadAtualizado);
+>>>>>>> front-end
+
+        /// <summary>
+        /// Deleta um usuário 
+        /// </summary>
+        void Deletar(int id);
+    }
+}
---
+++ back/OkEntrega.webApi/OkEntrega.webApi/OkEntrega.webApi/Repositorios/LeadRepository.cs
@@ -0,0 +1,58 @@
+﻿using OkEntrega.webApi.Contexts;
+using OkEntrega.webApi.Domains;
+using OkEntrega.webApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OkEntrega.webApi.Repositorios
+{
+    public class LeadRepository : ILeadRepository
+    {
+        OkEntregasContext ctx = new OkEntregasContext();
+
+<<<<<<< HEAD
+        public void Atualizar(int id, Lead leadAtualizado)
+=======
+        public void Atualizar(int id, UsuarioLead leadAtualizado)
+>>>>>>> front-end
+        {
+            throw new NotImplementedException();
+        }
+
+<<<<<<< HEAD
+        public Lead BuscarPorId(int id)
+=======
+        public UsuarioLead BuscarPorId(int id)
+>>>>>>> front-end
+        {
+            throw new NotImplementedException();
+        }
+
+<<<<<<< HEAD
+        public void Cadastrar(Lead novoLead)
+=======
+        public void Cadastrar(UsuarioLead novoLead)
+>>>>>>> front-end
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+<<<<<<< HEAD
+        public List<Lead> Listar()
+        {
+            throw new NotImplementedException();
+=======
+        public List<UsuarioLead> Listar()
+        {
+            return ctx.UsuarioLeads.ToList();
+>>>>>>> front-end
+        }
+    }
+}
