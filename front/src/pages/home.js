f02a072b84b7f1a09f204b7f594f363704b4bf80
joaovitorfreitas <joaovdlf@gmail.com>
2021-12-09 02:02:24 -0300

    Cpnj atualizado

--- front/src/pages/home.js
+++ front/src/pages/home.js
@@ -1,59 +1,85 @@
-import React, { Component } from 'react';
-// import axios from 'axios';
-
+import React, { useState,  } from 'react';
 import '../css/base-style.css'
 import '../css/home.css'
 import Side from '../components/side-bar'
 import Email from '../components/email'
+import axios from 'axios';
 
-export default class Login extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
+function Login () {
+    const [cpnj, setCNPJ] = useState("");
+    const [cpnjRazao, setCNPJRazao] = useState("");
+    const [cpnjTelefone, setCNPJTelefone] = useState("");
+    const [cpnjNome, setCNPJNome] = useState("");
+    const [params, setParams] = useState("")
 
-        }
-    }
 
-    render() {
-        return (
-            <main>
-                <Side/>
-                <Email/>
+
+
+    async function BuscarCnpj(event){
+        console.log("entrou")
+        event.preventDefault();
+
+        await axios.get(`https://www.receitaws.com.br/v1/cnpj/${params}`)
+        .then(rps => {
+            console.log(rps.data)
+            setCNPJ(
+                rps.data.email
+            )
+            setCNPJRazao(
+                rps.data.fantasia
+            )
+            setCNPJNome(
+                rps.data.nome
+            )
+            setCNPJTelefone(
+                rps.data.telefone
+            )
+        })
+        .catch(error => console.log(error))
+   }
+
+
+
+   return(
+    <main>
+        
+            <Side/>
+            <Email/>
                 <section className="content">
                     <div className="glass-home flex flex-collumn ai-flex-start jc-flex-start">
                         <h1>Digite um CNPJ</h1>
-                        <form className="pesquisa flex">
+                        <form className="pesquisa flex" onSubmit={BuscarCnpj}>
                             <div className="inputs-home flex flex-collumn">
                                 <label>CNPJ</label>
-                                <input type="text"></input>
+                                <input type="text" value={params} onChange={(event) => setParams(event.target.value)}></input>
                             </div>
                             <div className="flex ai-center ai-flex-end">
-                                <button className="btn-pesquisar flex ai-center jc-center"><i id="lupa" className="fas fa-search"></i>Buscar</button>
+                                <button className="btn-pesquisar flex ai-center jc-center"><i id="lupa" className="fas fa-search" type='submit'></i>Buscar</button>
                             </div>
                         </form>
                         <div className="resultado">
                             <div className="resultado-content flex flex-collumn ai-flex-start jc-space-eve">
                                 <div className="inputs-resultado flex ai-center">
                                     <label>E-mail :</label>
-                                    <input type="text"></input>
+                                    <input type="text" value={cpnj} onChange={(event) => setCNPJ(event.target.value)} name={cpnj} disabled></input>
                                     <div className="copy">
                                         <button>Copiar E-mail<i id="copy" className="fas fa-copy"></i></button>
                                     </div>
                                 </div>
                                 <div className="inputs-resultado flex ai-center">
                                     <label>Telefone :</label>
-                                    <input type="text"></input>
+                                    <input type="text" value={cpnjTelefone} onChange={(event) => setCNPJTelefone(event.target.value)} name={cpnjTelefone} disabled></input>
                                     <div className="call">
                                         <button>Ligar<i id="ligar" className="fas fa-phone"></i></button>
                                     </div>
                                 </div>
                                 <div className="inputs-resultado flex ai-center">
                                     <label>Razão Social :</label>
-                                    <input type="text"></input>
+                                    <input type="text"  value={cpnjNome} onChange={(event) => setCNPJNome(event.target.value)} name={cpnjNome} disabled></input>
                                 </div>
                                 <div className="inputs-resultado flex ai-center">
                                     <label>Nome Fantasia :</label>
-                                    <input type="text"></input>
+                                    <input type="text" value={cpnjRazao} onChange={(event) => setCNPJRazao(event.target.value)} name={cpnjRazao} disabled></input>
                                 </div>
                                 <div className="historico flex ai-center">
                                     <button className="flex ai-center jc-center"><i id="hist" className="fas fa-history"></i>Procurar no Histórico</button>
@@ -65,7 +91,16 @@ export default class Login extends Component{
                 <div className="bc1"></div>
                 <div className="bc2"></div>
             </main>
-        )
-    }
+   );
 
 }
+
+export default Login;
+
+
+
+    
+
+
+    
+
