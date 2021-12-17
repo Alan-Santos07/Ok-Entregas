baaae9af7d207a3ba53de7e812cabe74241d09ee
joaovitorfreitas <joaovdlf@gmail.com>
2021-12-17 10:25:58 -0300

    envio de email arrumado

--- front/src/components/email.js
+++ front/src/components/email.js
@@ -1,61 +1,47 @@
-import React, { useState  } from 'react';
+import React, { useState, useRef  } from 'react';
 import axios from 'axios';
+import emailjs from 'emailjs-com';
+
 
 import '../css/base-style.css'
 import '../css/email.css'
 
 function Email() {
-    const [NomeSeu, setNomeSeu] = useState('')
-    const [Endereco, setEndereçoSeu] = useState('')
+    const [Replay, setReplay] = useState('')
+        const form = useRef();
 
-    const [NomePara, setEndereçoPara] = useState('')
-    const [EnderecoPara, setEnderecoPara] = useState('')
+        function  EnviarEmail(e){
+            e.preventDefault();
 
-    const [Assunto, setAssunto] = useState('')
-    const [Mensagem, setMensagem] = useState('')
-
-        function  EnviarEmail(event){
-            event.preventDefault();
-
-            let EmailBody = {
-                from: {name: 'STILL SOLUTIONS', address: 'joaovdlf@gmail.com'},
-                to: {name: 'JOAO VITOR', address: EnderecoPara},
-                subject: Assunto,
-                message: Mensagem
-            };
-
-            axios.post("https://easymail.p.rapidapi.com/send", EmailBody, {
-            headers:{
-                'X-RapidAPI-Host': '    ',
-                'X-RapidAPI-Key': '56519e1a91msh35cec7200c6bc6ep1928fbjsnd6c4051505c8'
-            }
-           })
-           .then(Response =>{
-                console.log(Response.data)
-                setAssunto("")
-                setMensagem("")
-                setEnderecoPara("")
-
-           })
-           .catch(error => console.log(error))
+            emailjs.sendForm('gmailMessage', 'template_8u60xt9', form.current, 'user_u8LH9KP4dVVTzPWvSWtYb', {}, {
+                Headers :{
+                    "reply-to":"okentregas10@gmail.com"
+                }
+            } )
+              .then((result) => {
+                  console.log(result.text);
+              }, (error) => {
+                  console.log(error.text);
+              });
+              e.target.reset();
            }
 
         return (
             <section className="email-bar flex flex-collumn">
                 <div className="email-content flex flex-collumn ai-center">
                     <h1>Enviar E-mail</h1>
-                    <form className="email-form flex flex-collumn jc-space-btw" onSubmit={EnviarEmail}>
+                    <form className="email-form flex flex-collumn jc-space-btw" ref={form} onSubmit={EnviarEmail}>
                         <div className="inputs-email flex flex-collumn">
                             <label>Destinatário</label>
-                            <input type="text" value={EnderecoPara} onChange={(event) => setEnderecoPara(event.target.value)}></input>
+                            <input type="text"name="email"></input>
                         </div>
                         <div className="inputs-email flex flex-collumn">
                             <label>Assunto</label>
-                            <input type="text" value={Assunto} onChange={(event) => setAssunto(event.target.value)}></input>
+                            <input type="text" name="subject"></input>
                         </div>
                         <div className="inputs-email flex flex-collumn">
                             <label>Conteúdo</label>
-                            <textarea id="input-email" type="text" value={Mensagem} onChange={(event) => setMensagem(event.target.value)}></textarea>
+                            <textarea id="input-email" type="text" name="message"></textarea>
                         </div>
                         <div className="flex ai-center ai-flex-end">
                             <button className="btn-email flex ai-center jc-center" type='submit'><i id="email" className="fas fa-envelope"></i>Enviar E-mail</button>
